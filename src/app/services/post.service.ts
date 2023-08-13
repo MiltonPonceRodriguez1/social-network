540e2eafdb8c6b60559474e2b180fa692619142a
Milton Ponce <miltonponceipn@gmail.com>
2023-08-12 18:30:55 -0600

    post implementation done

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -19,4 +19,28 @@ export class PostService {
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
         return this._http.get(`${this.url}post`, {headers: headers});
     }
+
+    store( token: string, post: Post ): Observable<any> {
+        let headers = new HttpHeaders().set('Authorization', token);
+        let formData = new FormData();
+
+        formData.append('user_id', post.user_id.toString());
+        formData.append('plan_id', post.plan_id.toString());
+        formData.append('title', post.title);
+        formData.append('category', post.category);
+
+        if (post.description !== null) formData.append('description', post.description);
+        if (post.email !== null) formData.append('email', post.email);
+        if (post.company !== null) formData.append('company', post.company);
+        if (post.phone !== null) formData.append('phone', post.phone);
+        
+        if ( post.images !== null ) {
+            post.images.forEach((file, i) => {
+                formData.append(`file${i}`, file, file.name);
+            });
+        }
+
+        return this._http.post(`${this.url}post`, formData, {headers: headers});
+    }
+
 }
--- src/app/shared/post-new/post-new.component.ts
+++ src/app/shared/post-new/post-new.component.ts
@@ -1,22 +1,28 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { UserService } from '../../services/user.service';
+import { PostService } from '../../services/post.service';
 
 import { Post } from '../../interfaces/post.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-new',
   templateUrl: './post-new.component.html',
-  styleUrls: ['./post-new.component.css']
+  styleUrls: ['./post-new.component.css'],
+  providers: [ UserService, PostService ],
 })
 export class PostNewComponent implements OnChanges {
   @Input() public plan: number = 0;
   public identity: any;
   public token: string;
   public post: Post;
+  public response: any;
 
   constructor (
     private _userService: UserService,
+    private _postService: PostService,
+    private _router: Router,
   ) {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
@@ -45,10 +51,28 @@ export class PostNewComponent implements OnChanges {
   }
 
   onChange(event: any) {
-    this.post.images = Array.from(event.target.files);    
+    this.post.images = Array.from(event.target.files);
+    console.log(this.post.images);
+        
   }
 
   onSubmit( form: any ): void {
+    this.response = null;
     console.log(this.post);
+
+    this._postService.store( this.token, this.post ).subscribe(
+      response => {
+        if ( response.status === 'success' ) {
+          console.log(response);
+          this.response = response;
+          setTimeout(() => { this._router.navigate(['/marketplace']); }, 2500);
+        }
+        
+      },
+      error => {
+        this.response = error.error;
+        console.log(<any>error);
+      }
+    );
   }
 }
